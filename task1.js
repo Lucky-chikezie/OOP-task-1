19226a0c40f2cb6306819e1747f0bb1ddd599509
Lucky-Chikezie <ugwuchikezielucky@gmail.com>
2026-04-12 07:16:32 +0100

    code001

---
+++ task1.js
@@ -0,0 +1,51 @@
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+    this.isAvailable = true;
+  }
+}
+
+class Member {
+  constructor(name) {
+    this.name = name;
+    this.borrowedBooks = [];
+  }
+
+  borrow(book) {
+    if (book.isAvailable) {
+      book.isAvailable = false;
+      this.borrowedBooks.push(book);
+      console.log(`${this.name} borrowed ${book.title}`);
+    } else {
+      console.log(`${book.title} is not available`);
+    }
+  }
+
+  return(book) {
+    book.isAvailable = true;
+    this.borrowedBooks = this.borrowedBooks.filter(b => b !== book);
+    console.log(`${this.name} returned ${book.title}`);
+  }
+}
+
+class Library {
+  constructor() {
+    this.books = [];
+    this.members = [];
+  }
+
+  addBook(book) { this.books.push(book); }
+  addMember(member) { this.members.push(member); }
+}
+
+// Demo
+const library = new Library();
+const book1 = new Book("Math 101", "Prof Eze");
+const member1 = new Member("Lucky");
+
+library.addBook(book1);
+library.addMember(member1);
+
+member1.borrow(book1);
+member1.return(book1);
